56bc6159fe9ea881480e95fe3f8196c72f8b248f
DarinelGuillen <darinel.escobar@outlook.com>
2024-06-02 10:36:49 -0600

    [update] header animations

--- src/components/molecules/Header.jsx
+++ src/components/molecules/Header.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import dumbbellIcon from '../../assets/images/dumbbell.png'; // Asegúrate de que esta ruta sea correcta
+import '../../assets/css/Header.css';
 
 function Header() {
   return (
-    <div className="relative mb-8 text-center text-white">
-      <img src={dumbbellIcon} alt="Dumbbell Icon" className="top-0 left-0 w-12 h-12" />
-      <div className="pt-6">
-        <h1 className="mt-6 font-GA text-4xl">FitSS Tracker App</h1>
-        <p className="mt-4 text-lg">The simplest tracking app for training</p>
+    <div className="text-white header-container">
+        <img src={dumbbellIcon} alt="Dumbbell Icon" />
+      <div className="flex justify-center items-center">
+        <h1 className="font-GA">FITSS TRACKER APP</h1>
       </div>
+      <p className="leFadeInLeft"><span>The simplest tracking app for training</span></p>
     </div>
   );
 }
